5910a0e1efdc8428e477f33505bd36e51d9bd1a9
winter.kwon <winter.kwon@gbike.io>
2023-10-16 21:36:58 +0900

    composite 예시코드

--- composite/main.go
+++ composite/main.go
@@ -7,26 +7,26 @@ func main() {
 	scooter2 := &obj.Scooter{ModelName: "max plus"}
 	scooter3 := &obj.Scooter{ModelName: "K2"}
 
-	segwayIoT := &obj.IoT{
-		IoTName: "Segway IoT",
+	segway := &obj.Company{
+		IoTName: "Segway",
 	}
 
-	segwayIoT.AddIot(scooter1)
-	segwayIoT.AddIot(scooter2)
+	segway.AddPM(scooter1)
+	segway.AddPM(scooter2)
 
-	msIoT := &obj.IoT{
-		IoTName: "Microworks IoT",
+	msIoT := &obj.Company{
+		IoTName: "Microworks",
 	}
 
-	gbikeIoT := &obj.IoT{
-		IoTName: "Gbike IoT",
+	gbike := &obj.Company{
+		IoTName: "Gbike",
 	}
 
-	gbikeIoT.AddIot(segwayIoT)
-	gbikeIoT.AddIot(msIoT)
+	gbike.AddPM(segway)
+	gbike.AddPM(msIoT)
 
-	msIoT.AddIot(scooter3)
+	msIoT.AddPM(scooter3)
 
-	gbikeIoT.Produce()
+	gbike.Produce()
 
 }
--- composite/obj/composite.go
+++ composite/obj/composite.go
@@ -2,18 +2,18 @@ package obj
 
 import "fmt"
 
-type IoT struct {
+type Company struct {
 	PMs     []PersonalMobility
 	IoTName string
 }
 
-func (g *IoT) Produce() {
+func (g *Company) Produce() {
 	fmt.Printf("%s can produce below models.\n", g.IoTName)
 	for _, composite := range g.PMs {
 		composite.Produce()
 	}
 }
 
-func (g *IoT) AddIot(iot PersonalMobility) {
-	g.PMs = append(g.PMs, iot)
+func (g *Company) AddPM(pm PersonalMobility) {
+	g.PMs = append(g.PMs, pm)
 }
